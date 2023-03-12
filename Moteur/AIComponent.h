b6a88e2ad08f82f54505935df6692f3241426e7c
Ademola Adedeji <ademola.adedeji@hotmail.com>
2023-03-12 18:20:51 -0400

    Added documentation for AI implementation

--- Moteur/AIComponent.h
+++ Moteur/AIComponent.h
@@ -15,13 +15,22 @@ struct AIComponent : Component
 public:
 	AIComponent() : ComponentType("AIComponent"), transformComponent(nullptr) {}
 
+	// Trigger radius between player and AI agent
 	float radius;
+
+	// The speed
 	float speed;
+
+	// The behavior type
 	BehaviorType type;
+
+	// The agent's transformation component.
 	TransformComponent* transformComponent;
 
+	// The destructor
 	~AIComponent() {};
 
 private:
+	// The component type
 	std::string ComponentType;
 };
